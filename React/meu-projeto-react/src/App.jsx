de4f02421cb2d1ce430b331d03e73d23a21f1f65
Brener Araujo <brener.araujo@engegraph.com.br>
2023-11-21 12:29:23 -0300

    Curso React

---
+++ React/meu-projeto-react/src/App.jsx
@@ -0,0 +1,8 @@
+//import BuscarDados from "./components/BuscarDados";
+import MeuContador from "./components/MeuContadorCss";
+
+export default function App() {
+  return (
+    <MeuContador />
+  )
+}
